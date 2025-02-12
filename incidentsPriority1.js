307975c0c49164349ebafadafc4a9acbcbb2cfdd
bernicetempleman2 <bernice.templeman2@GMAIL.COM>
2025-02-12 15:15:56 -0500

    Create incidentsPriority1.js

---
+++ incidentsPriority1.js
@@ -0,0 +1,11 @@
+//print all incident numbers with priority of 1
+
+var incidentGR = new GlideRecord('incident');
+
+incidentGR.addQuery('priority', 1);
+incidentGR.query();
+
+//lop to print numbers
+while (incidentGR.next()){
+  gs.print(incidentGR.number);
+}
