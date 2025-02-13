8a664dafc6ea50a7b7b6e90ed8575cccec4b964d
bernicetempleman2 <bernice.templeman2@GMAIL.COM>
2025-02-13 15:28:00 -0500

    Create incidentsOrderByShortDescription.js

---
+++ incidentsOrderByShortDescription.js
@@ -0,0 +1,8 @@
+//orderBy
+
+var incidentGR = new GlideRecord('incident');
+incidentGR.orderBy('short_description');
+incidentGR.query();
+while(incidentGR.next()){
+  gs.print(incidentGR.number + ' '+ incidentGR.short_description);
+}
